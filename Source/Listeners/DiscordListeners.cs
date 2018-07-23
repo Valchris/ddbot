ba97cd8c6b2d8998239455ff8abe48c45839d762
Daniel Leblond <dleblond@microsoft.com>
2018-07-23 14:50:35 -0700

    Add History stub

---
+++ Source/Listeners/DiscordListeners.cs
@@ -0,0 +1,48 @@
+﻿using DDBot.Services;
+using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDBot.Listeners
+{
+    public class DiscordListeners : IDiscordListeners
+    {
+        private readonly SentimentService sentimentService;
+        private readonly SentimentHistoryService sentimentHistoryService;
+
+        public DiscordListeners(SentimentService sentimentService, SentimentHistoryService sentimentHistory)
+        {
+            this.sentimentService = sentimentService;
+        }
+
+        public Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public async Task MessageReceived(SocketMessage message)
+        {
+            switch(message.Content)
+            {
+                case "!ping":
+                    await message.Channel.SendMessageAsync("Pong!");
+                    break;
+                case "!sentiment":
+
+                    break;
+                case "!analysis":
+
+                    break;
+                default:
+                    var result = await sentimentService.AnalyzeMessage(new List<SocketMessage>() { message });
+                    return;
+            }
+        }
+
+    }
+}
---
+++ Source/Services/SentimentHistoryService.cs
@@ -0,0 +1,28 @@
+﻿using DDBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDBot.Services
+{
+    public class SentimentHistoryService
+    {
+        private readonly List<SentimentScore> scores;
+
+        public SentimentHistoryService()
+        {
+            this.scores = new List<SentimentScore>();
+        }
+
+        public void StoreMessage(SentimentScore score)
+        {
+            this.scores.Add(score);
+            // Add logic for cold storage here
+
+            // Add remove message logic for >24h
+        }
+
+    }
+}
