0b35a55affa202ce12e3e14e2ab87029dc94b218
Danielle Fetsch <daniellefetsch@outlook.com>
2018-07-26 21:26:58 -0700

    Chart embed functionality and UX update

--- Source/Services/ChartService.cs
+++ Source/Services/ChartService.cs
@@ -20,50 +20,50 @@ namespace DDBot.Services
                 {
                     chart.Series["Series1"].Points.Add(datapoint);
 
-                    if (datapoint.YValues[0] <= 25)
+                    if (datapoint.YValues[0] < 40)
                     {
                         chart.Series["Series1"].Points[dpCount].Color = Color.DarkRed;
                         chart.Series["Series1"].Points[dpCount].BackSecondaryColor = Color.Red;
-                        chart.Series["Series1"].BackGradientStyle = GradientStyle.TopBottom;
                     }
-                    else if (datapoint.YValues[0] <= 50)
-                    {
-                        chart.Series["Series1"].Points[dpCount].Color = Color.DarkOrange;
-                        chart.Series["Series1"].Points[dpCount].BackSecondaryColor = Color.Orange;
-                        chart.Series["Series1"].BackGradientStyle = GradientStyle.TopBottom;
-                    }
-                    else if (datapoint.YValues[0] <= 75)
+                    else if (datapoint.YValues[0] <= 60)
                     {
                         chart.Series["Series1"].Points[dpCount].Color = Color.Yellow;
                         chart.Series["Series1"].Points[dpCount].BackSecondaryColor = Color.LightGoldenrodYellow;
-                        chart.Series["Series1"].BackGradientStyle = GradientStyle.TopBottom;
                     }
                     else
                     {
                         chart.Series["Series1"].Points[dpCount].Color = Color.ForestGreen;
                         chart.Series["Series1"].Points[dpCount].BackSecondaryColor = Color.Green;
-                        chart.Series["Series1"].BackGradientStyle = GradientStyle.TopBottom;
                     }
                     dpCount++;
                 }
 
                 chart.Series["Series1"].ChartArea = "ChartArea1";
                 chart.Series["Series1"].ChartType = SeriesChartType.Bar;
-                chart.Series["Series1"]["DrawingStyle"] = "Cylinder";
-                
-                chart.Series["Series1"].Font = new Font("Segoe UI", 8, FontStyle.Bold);
-                //chart.Series["Series1"].Sort(PointSortOrder.Ascending, "X");
+                chart.Series["Series1"]["DrawingStyle"] = "Emboss";
+
+                chart.Series["Series1"].Font = new Font("Segoe UI", 12, FontStyle.Bold);
 
                 chart.ChartAreas["ChartArea1"].AxisX.MajorGrid.LineWidth = 0;
                 chart.ChartAreas["ChartArea1"].AxisX.Minimum = 0.5;
                 chart.ChartAreas["ChartArea1"].AxisX.Maximum = dpCount+0.5;
-                chart.ChartAreas["ChartArea1"].AxisX.Interval = 0.5;
+                chart.ChartAreas["ChartArea1"].AxisX.Interval = 0.50;
+                chart.ChartAreas["ChartArea1"].AxisX.LineColor = Color.White;
+                chart.ChartAreas["ChartArea1"].AxisX.IsLabelAutoFit = false;
+                chart.ChartAreas["ChartArea1"].AxisX.LabelStyle.ForeColor = Color.White;
 
                 chart.ChartAreas["ChartArea1"].AxisY.MajorGrid.LineWidth = 0;
                 chart.ChartAreas["ChartArea1"].AxisY.Maximum = 100;
-                chart.ChartAreas["ChartArea1"].AxisY.Title = "Debbie %";
+                chart.ChartAreas["ChartArea1"].AxisY.Interval = 10;
+                chart.ChartAreas["ChartArea1"].AxisY.Title = "Sentiment %";
+                chart.ChartAreas["ChartArea1"].AxisY.TitleForeColor = Color.White;
+                chart.ChartAreas["ChartArea1"].AxisY.TitleFont = new Font("Segoe UI", 12, FontStyle.Bold);
+                chart.ChartAreas["ChartArea1"].AxisY.LineColor = Color.White;
+                chart.ChartAreas["ChartArea1"].AxisY.LabelStyle.ForeColor = Color.White;
 
-                chart.ChartAreas["ChartArea1"].BackColor = Color.Transparent;
+                chart.Size = new Size(600, 350);
+                chart.BackColor = Color.FromArgb(37, 39, 42);
+                chart.ChartAreas["ChartArea1"].BackColor = Color.FromArgb(57, 59, 65);
                 chart.ChartAreas["ChartArea1"].BorderDashStyle = ChartDashStyle.NotSet;
 
                 chart.SaveImage("a_mypic.png", System.Drawing.Imaging.ImageFormat.Png);
